1de13372c52b4b26bad8cc9912423206f1456511
Oscar Herrera <oskr916@hotmail.com>
2022-06-17 21:35:17 -0500

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarfijaComponent } from './components/navbarfija/navbarfija.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistroComponent,
-    NosotrosComponent
+    NosotrosComponent,
+    FooterComponent,
+    NavbarfijaComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
