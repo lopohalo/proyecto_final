73ba7deb9d1f87db04e1eb40d98a2038099c7f46
SrJuan <jsebg.ruedat@gmail.com>
2022-06-18 00:34:05 -0500

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
-import { RegistroComponent } from './components/registro/registro.component';
+
 
 const routes: Routes = [
-  {path: "registro",component:RegistroComponent},
   {path: "nosotros", component:NosotrosComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistroComponent } from './components/registro/registro.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarfijaComponent } from './components/navbarfija/navbarfija.component';
@@ -13,7 +11,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    RegistroComponent,
     NosotrosComponent,
     FooterComponent,
     NavbarfijaComponent,
